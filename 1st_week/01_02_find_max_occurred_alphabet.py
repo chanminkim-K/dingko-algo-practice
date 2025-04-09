0914ccdcbfee5b5ee17f04ddadf39a01eb827531
chanminkim-K <snoopy0813@naver.com>
2025-04-09 15:42:33 +0900

    Solve: 최댓값 찾기, 최빈값 찾기 수정

--- 1st_week/01_02_find_max_occurred_alphabet.py
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -1,45 +1,44 @@
-# def find_max_occurred_alphabet(string):
-#     alphabet_occurrence_array = [0] * 26
-#
-#     for char in string:
-#         if not char.isalpha():
-#             continue
-#         alphabet_occurrence_array[ord(char) - 97] += 1
-#
-#     max_occurrence = 0
-#
-#     # for index in range(len(alphabet_occurrence_array)):
-#     #     alphabet_occurrence = alphabet_occurrence_array[index]
-#     #     if alphabet_occurrence > max_occurrence:
-#     #         max_occurrence = alphabet_occurrence
-#
-#     max_occurrence = max(alphabet_occurrence_array)
-#
-#     max_alphabet_index = [i for i, v in enumerate(alphabet_occurrence_array) if v == max_occurrence]
-#     answer = [chr(n + 97) for n in max_alphabet_index]
-#     return answer
-#
-#
-# result = find_max_occurred_alphabet
-# print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
-# print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
-# print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
+def find_max_occurred_alphabet(string):
+    alphabet_occurrence_array = [0] * 26
 
-def solution(array):
-    number_array = [0] * 1000
-    for num in array:
-        number_array[num] += 1
+    for char in string:
+        if not char.isalpha():
+            continue
+        alphabet_occurrence_array[ord(char) - 97] += 1
 
-    max_num = max(number_array)
-    max_num_index = [i for i, v in enumerate(number_array) if v == max_num]
+    max_occurrence = 0
 
-    if len(max_num_index) > 1:
-        return -1
-    else:
-        answer = max_num_index[0]
+    for index in range(len(alphabet_occurrence_array)):
+        alphabet_occurrence = alphabet_occurrence_array[index]
+        if alphabet_occurrence > max_occurrence:
+            max_occurrence = alphabet_occurrence
+
+    max_alphabet_index = [i for i, v in enumerate(alphabet_occurrence_array) if v == max_occurrence]
+    answer = [chr(n + 97) for n in max_alphabet_index]
     return answer
 
-result = solution
-print(result([1, 2, 3, 3, 3, 4]))
-print(result([1, 1, 2, 2]))
-print(result([1]))
+
+result = find_max_occurred_alphabet
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
+
+## 프로그래머스 최빈값 찾기 문제
+# def solution(array):
+#     number_array = [0] * 1000
+#     for num in array:
+#         number_array[num] += 1
+#
+#     max_num = max(number_array)
+#     max_num_index = [i for i, v in enumerate(number_array) if v == max_num]
+#
+#     if len(max_num_index) > 1:
+#         return -1
+#     else:
+#         answer = max_num_index[0]
+#     return answer
+#
+# result = solution
+# print(result([1, 2, 3, 3, 3, 4]))
+# print(result([1, 1, 2, 2]))
+# print(result([1]))
