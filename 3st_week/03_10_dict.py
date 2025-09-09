23172950792a186d7d2435b12f3e04a69ba930c9
chanminkim-K <snoopy0813@naver.com>
2025-09-10 00:12:41 +0900

    3주차 복습

--- 3st_week/03_10_dict.py
+++ 3st_week/03_10_dict.py
@@ -1,14 +1,13 @@
 class LinkedTuple:
     def __init__(self):
-        self.items = []
-
+        self.item = []
 
     def add(self, key, value):
-        self.items.append((key, value))
+        self.item.append((key, value))
 
     def get(self, key):
-        for k, v in self.items:
-            if key == k:
+        for k, v in self.item:
+            if k == key:
                 return v
 
 class LinkedDict:
@@ -26,7 +25,41 @@ class LinkedDict:
         return self.items[index].get(key)
 
 
-my_dict = LinkedDict()
-my_dict.put("333", 7)
-my_dict.put("77", 6)
-print(my_dict.get("77"))  # 3이 반환되어야 합니다!
+class Dict:
+    def __init__(self):
+        self.items = [None] * 8
+
+    def put(self, key, value):
+        index = hash(key) % len(self.items)
+        self.items[index] = value
+
+    def get(self, key):
+        index = hash(key) % len(self.items)
+        return self.items[index]
+
+
+my_dict = Dict()
+my_dict.put("1", 1)
+my_dict.put("2", 2)
+my_dict.put("3", 3)
+my_dict.put("4", 4)
+my_dict.put("5", 5)
+my_dict.put("6", 6)
+my_dict.put("7", 7)
+my_dict.put("8", 8)
+my_dict.put("9", 9)
+print(my_dict.items)
+print("충돌 해결 전 ", my_dict.get("1"))
+
+
+my_dict2 = LinkedDict()
+my_dict2.put("1", 1)
+my_dict2.put("2", 2)
+my_dict2.put("3", 3)
+my_dict2.put("4", 4)
+my_dict2.put("5", 5)
+my_dict2.put("6", 6)
+my_dict2.put("7", 7)
+my_dict2.put("8", 8)
+my_dict2.put("9", 9)
+print("충돌 해결 후 ", my_dict2.get("1"))  # 3이 반환되어야 합니다!
